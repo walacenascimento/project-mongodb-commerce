d70cc84a55f2870953e557ae02912c9c57739230
walacenascimento <wlacens@hotmail.com>
2021-11-28 15:57:56 -0300

    Resolve o desafio 13

---
+++ challenges/desafio13.js
@@ -0,0 +1,14 @@
+// 13.1
+db.produtos.updateMany(
+  { valoresNutricionais: {
+    $elemMatch: {
+    percentual: { $gte: 40 },
+    tipo: "sódio",
+    } } },
+  { $push: { tags: "muito sódio" } },
+);
+// 13.2
+db.produtos.find(
+  {},
+  { _id: 0, nome: 1, tags: 1 },
+);
